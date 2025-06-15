d151709504fd72d7d42c4223846d59025355cabd
Andy-Ops254 <ghostandy254@outlook.com>
2025-06-15 10:46:19 +0300

    complete object lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={
+    name: "Andrew Rimongi",
+    streetAddress: "4th south avenue,Kahawa sukari",
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj={...obj};
+    newObj[key]=value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj
+}
+
+function deleteFromEmployeeByKey (employee,key) {
+    const deleteKey={...employee};
+    delete deleteKey[key]
+    return deleteKey
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee,key) {
+    delete employee[key];
+    return employee;
+}
